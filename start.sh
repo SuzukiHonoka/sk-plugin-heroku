fec3be068190f520d927992d6cf838336ecc6c33
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2019-05-02 16:57:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -44,7 +44,7 @@ cat <<-EOF > /home/Software/ss.json
     "server_port":8080,
     "local_port":1080,
     "password":"${S_PW}",
-    "Method":"${S_Method}",
+    "method":"${S_Method}",
     "mode":"tcp_only",
     "timeout":300,
     "plugin":"/home/Software/v2ray-plugin_linux",
