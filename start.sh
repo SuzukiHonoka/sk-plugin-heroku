cc008c703db442135dd63ce4cd6c3d3790853021
铃木和花 <49331402+SuzukiHonoka@users.noreply.github.com>
2019-09-22 13:02:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -32,12 +32,13 @@ sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
 sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 
 
-wget --no-check-certificate -qO '/tmp/demo.tar.gz' "https://github.com/Dark11296/sk-plugin-heroku/raw/master/demo.tar.gz"
-wget --no-check-certificate -qO '/tmp/v2ray-plugin.tar.gz' "https://github.com/Dark11296/sk-plugin-heroku/raw/master/v2ray-plugin-linux-$SYS_Bit.tar.gz"
+wget --no-check-certificate -qO '/tmp/demo.tgz' "https://github.com/SuzukiHonoka/sk-plugin-heroku/raw/master/demo.tgz"
+wget --no-check-certificate -qO '/tmp/v2ray-plugin.tar.gz' "https://github.com/SuzukiHonoka/sk-plugin-heroku/raw/master/v2ray-plugin-linux-$SYS_Bit.tar.gz"
 
-tar xvf /tmp/demo.tar.gz -C /wwwroot
+tar xvf /tmp/demo.tgz -C /wwwroot
 tar xvf /tmp/v2ray-plugin.tar.gz -C /home/Software
-chmod +x /home/Software/*
+chmod +x -R /home/Software/
+chmod -R 775 /wwwroot
 
 cat <<-EOF > /home/Software/ss.json
 {
